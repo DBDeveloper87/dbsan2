b8831478e2d3cc43ded59dd5036d444da1ce7dea
DBDeveloper87 <patrick.vellia@gmail.com>
2022-04-09 18:23:13 -0400

    updated production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'dbsan2-production.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'dbsan.org}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = { 
     address:        'smtp.sendgrid.net', 
