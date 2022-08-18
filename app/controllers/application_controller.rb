4fb35d18db74b8502c0f21837f2f9fb7e9518724
Patrick Vellia <patrick.vellia@gmail.com>
2022-08-18 17:40:34 -0500

    working on reCAPTCHA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
-	#before_action :authenticate_user!
+	require 'net/https'
 	before_action :set_social
 	helper_method :admin?
+	RECAPTCHA_MINIMUM_SCORE = 0.5
 	
+
 	def admin?
 		current_user&.admin == true
 	end
@@ -19,8 +21,14 @@ class ApplicationController < ActionController::Base
 
 	def set_social
 		@networks = SocialNetwork.all
-	end
+	end	
 	
-	
-	
+	def verify_recaptcha?(token, recaptcha_action)
+    	secret_key = Rails.application.credentials.recaptcha[:SECRET_KEY]
+
+    	uri = URI.parse("https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{token}")
+    	response = Net::HTTP.get_response(uri)
+    	json = JSON.parse(response.body)
+    	json['success'] && json['score'] > RECAPTCHA_MINIMUM_SCORE && json['action'] == recaptcha_action
+    end	
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,11 @@ class ContactsController < ApplicationController
     	@contact = Contact.new(params[:contact])
     	@contact.request = request
     	
+    	unless verify_recaptcha?(params[:recaptcha_token], 'contact')
+      		flash.now[:error] = t('recaptcha.errors.verification_failed')
+      		return redirect_to root_path
+    	end
+    		
     	respond_to do |f|
     		if @contact.deliver
       			f.html { redirect_to message_sent_path, notice: "Message sent" }
