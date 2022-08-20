0ba42b448bbdcd035e84df0e8ba4700556e6c52b
Patrick Vellia <patrick.vellia@gmail.com>
2022-08-20 18:50:47 -0500

    moving ahead

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
       		flash.now[:error] = t('recaptcha.errors.verification_failed')
       		return redirect_to root_path
     	end
-    		
+    	
     	respond_to do |f|
     		if @contact.deliver
       			f.html { redirect_to message_sent_path, notice: "Message sent" }
