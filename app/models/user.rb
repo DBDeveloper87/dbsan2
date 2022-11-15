4560baa607642f906276f7c2e0174abedcef2b2c
Patrick Vellia <patrick.vellia@gmail.com>
2022-11-15 09:21:49 -0500

    turned off account confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :encryptable
+         :encryptable
 
   has_one :profile
   accepts_nested_attributes_for :profile
