40400cde117a83db22e46b968e36e90f6bc2e03b
Patrick Vellia <patrick.vellia@gmail.com>
2022-11-24 14:12:01 -0600

    added photo association to the challenge_milestones

--- app/controllers/challenge_milestones_controller.rb
+++ app/controllers/challenge_milestones_controller.rb
@@ -1,13 +1,19 @@
 class ChallengeMilestonesController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_admin
 	before_action :get_challenge, only: [:index, :new, :create]
+	before_action :set_milestone, only: [:edit, :update]
 
 	def index
 		@exercise = @challenge.milestones.where(milestone_type: "Exercise").all
-		@fundraising = @challenge.milestones.where(milestone_type: "Fundraising").all.order(goal: :desc)
+		@fundraising = @challenge.milestones.where(milestone_type: "Fundraising").all.order(goal: :asc)
 	end
 
 	def new
 		@milestone = @challenge.milestones.build
+	end
+
+	def edit
 	end
 
 	def create
@@ -18,12 +24,24 @@ class ChallengeMilestonesController < ApplicationController
 		end
 	end
 
+	def update
+		if @milestone.update(create_params)
+			redirect_to challenge_milestones_path(@challenge)
+		end
+	end
+
 	private
 		def get_challenge
 			@challenge = Challenge.find_by(id: params[:challenge_id])
 		end
 
+		def set_milestone
+			@challenge = Challenge.find_by(id: params[:challenge_id])
+			@milestone = @challenge.milestones.find(params[:id])
+		end
+
 		def create_params
-			params.require(:challenge_milestone).permit(:milestone_type, :name, :prize, :goal, :goal_unit)
+			params.require(:challenge_milestone).permit(:milestone_type, :name, :prize, :goal, :goal_unit, 
+				photo_attributes: [:file, :image_type])
 		end
 end
--- app/models/challenge_milestone.rb
+++ app/models/challenge_milestone.rb
@@ -1,4 +1,6 @@
 class ChallengeMilestone < ApplicationRecord
 	belongs_to :challenge
 	has_many :participant_milestones
+	has_one :photo
+	accepts_nested_attributes_for :photo
 end
