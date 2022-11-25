a2a1d7a86511f0e13e66c5a0ebf68c64c2d6f681
Patrick Vellia <patrick.vellia@gmail.com>
2022-11-25 15:38:25 -0600

    testing routing

--- lib/channel_subdomains.rb
+++ lib/channel_subdomains.rb
@@ -2,14 +2,21 @@ class ChannelSubdomains
 	def self.matches? request
 		channels = Channel.all
 		subdomains = []
+		domains = []
 		channels.each do |c|
 			subdomains.append(c.subdomain.slug)
+			domains.append(c.domain_host)
 		end
+		domains = domains.uniq
 
 		if Rails.env == "development"
 			request.domain == "example.com" and request.subdomain.in?(subdomains)
-		else
-			request.domain == "dbsan.org" and request.subdomain.in?(subdomains)
+		elsif Rails.env == "production"
+			if request.domain == "dbsan.org"
+				request.subdomain.in?(subdomains)
+			else
+				request.domain.in?(domains)
+			end
 		end
 	end
 end
