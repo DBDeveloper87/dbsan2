9c676b06473d9e8435453cc1273b3600956b419f
Patrick Vellia <patrick.vellia@gmail.com>
2023-03-05 21:58:29 -0600

    fixed uploader issue

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
 	belongs_to :channel
-	belongs_to :playlist
+	belongs_to :playlist, optional: true
 	has_many :text_tracks
 	has_many :cue_blocks, through: :text_tracks
 
