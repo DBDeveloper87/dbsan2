d61295947b80a826f49698340807c9152b4fbc20
Patrick Vellia <patrick.vellia@gmail.com>
2023-03-07 19:23:51 -0600

    channel layout for video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,7 @@ class VideosController < ApplicationController
 	before_action :authenticate_user!, only: [:index, :new, :edit, :create, :update]
 	before_action :get_channel
 	before_action :set_video, only: [:show, :edit, :update]
-	#layout "channel", only: [:show]
+	layout "channel", only: [:show]
 
 	def index
 		@videos = @channel.videos.all
@@ -64,7 +64,7 @@ class VideosController < ApplicationController
 			@meta_title = "#{@video.title} | #{@video.channel.name}"
 			@meta_og_type = "video.movie"
 			@meta_og_url = video_url(@video)
-			@meta_og_image = "https://storage.googleapis.com/dbsan-public/poster.png"
+			#@meta_og_image = "https://storage.googleapis.com/dbsan-public/poster.png"
 		end
 		
 
