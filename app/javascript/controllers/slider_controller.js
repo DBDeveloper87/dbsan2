14be312b2fbf35afbcdfed1448af6e7aed5f99f4
Patrick Vellia <patrick.vellia@gmail.com>
2023-03-23 07:57:10 -0500

    added valuetext

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+	static values = {
+		text: String
+	}
+
+	connect() {
+		if (this.textValue == "Percent") {
+			this.element.setAttribute("aria-valuetext", (this.element.value * 100) + "%")
+		}
+	}
+
 	maintainFocus() {
 		this.element.focus()
+	}
+
+	updateValueText() {
+		if (this.textValue == "Percent") {
+			this.element.setAttribute("aria-valuetext", (this.element.value * 100) + "%")
+		}
 	}
 }
