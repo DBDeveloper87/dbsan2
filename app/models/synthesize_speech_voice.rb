39985dd9700f5a9593a017b8ea954208b3c8ffb1
Patrick Vellia <patrick.vellia@gmail.com>
2023-09-27 18:53:53 -0500

    latest

--- app/models/synthesize_speech_voice.rb
+++ app/models/synthesize_speech_voice.rb
@@ -3,4 +3,8 @@ class SynthesizeSpeechVoice < ApplicationRecord
 
   enum voice_type: {neural2: 0}
   enum ssml_gender: {male: 0, female: 1, neutral: 2}
+
+  def short_code
+    self.language.short_code
+  end
 end
